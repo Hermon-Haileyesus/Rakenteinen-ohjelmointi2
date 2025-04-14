5e063341159d89d86efb6893b1e00110b0850381
Hermon <hg2397@edu.turku.fi>
2025-04-14 09:49:01 +0300

    there are two option

--- Tehtävä16d_moduuli.py
+++ Tehtävä16d_moduuli.py
@@ -25,3 +25,32 @@ def vuoden_paivat(year, viikonpaiva):
             nykyinen += datetime.timedelta(days=1)
         return paivat
         
+
+
+        """"import locale
+from datetime import datetime, timedelta
+
+# Aseta kieli suomeksi
+locale.setlocale(locale.LC_TIME, "fi_FI")
+
+def paivan_viikonpaiva(pvm):
+    try:
+        paiva = datetime.strptime(pvm, "%d.%m.%Y")
+        return paiva.strftime("%A")  # Viikonpäivä suomeksi
+    except ValueError:
+        return "Virheellinen päivämäärä!"
+
+def vuoden_paivat(year, viikonpaiva):
+    try:
+        viikonpaiva = viikonpaiva.lower().capitalize()
+        paivat = []
+        alku = datetime(year, 1, 1)
+        loppu = datetime(year, 12, 31)
+        nykyinen = alku
+        while nykyinen <= loppu:
+            if nykyinen.strftime("%A") == viikonpaiva:
+                paivat.append(nykyinen.strftime("%d.%m.%Y"))
+            nykyinen += timedelta(days=1)
+        return paivat
+    except:
+        return "Virheelliset syötteet!""""
