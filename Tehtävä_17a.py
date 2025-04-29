1f2e1950f3965ea6529380b3609afd0ceaf3b421
Hermon <hg2397@edu.turku.fi>
2025-04-29 08:24:33 +0300

    just raise valueError

--- Tehtävä_17a.py
+++ Tehtävä_17a.py
@@ -1,18 +1,16 @@
-
-    
 numerot = []
 while True:
     try:
        luku = (input("Anna luku: "))
        if not luku:
-           print("Syöte ei voi olla tyhjä!")
-           continue
+          raise ValueError("Syöte ei voi olla tyhjä!")
+    
        luku = float(luku)
        numerot.append(luku)
        print(f"Suurin luku: {max(numerot)}")
        print(f"Pienin luku: {min(numerot)}")
        print(f"Lukujen keskiarvo: {sum(numerot) / len(numerot):.2f}")
-    except ValueError:
-        print("Virheellinen syöte, yritä uudelleen!")
+    except ValueError as e:
+        print(f"Virheellinen syöte, {e}")
     
     
